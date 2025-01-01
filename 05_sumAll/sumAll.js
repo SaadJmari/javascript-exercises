7b559325eec580295dc3e1ca7df1ef5c56c63881
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2025-01-01 16:15:00 +0100

    Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR"
+    }
+    let sum = 0;
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    else {
+        for (let i = b; i <= a; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 
 };
 
