1f2cba8ea48de206df7c2c234920dd1263829167
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2025-01-01 11:03:27 +0100

    Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (text) {
+    let string = ""
+    for (i = text.length; i >= 0; i--) {
+        string += text.charAt(i - 1)
+    }
+    return string;
 };
 
 // Do not edit below this line
