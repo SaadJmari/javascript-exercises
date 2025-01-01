7d48cd83e97856ba6e90ecbdf9e6a0911da3373c
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2025-01-01 15:39:57 +0100

    Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (list, num, num2, num3, num4) {
+    let newList = [];
+    for (i = 0; i < list.length; i++) {
+        if (list[i] !== num && list[i] !== num2 && list[i] !== num3 && list[i] !== num4) {
+            newList.push(list[i])
+        }
+        else {
+            continue;
+        }
+    }
+    return newList;
 };
 
 // Do not edit below this line
