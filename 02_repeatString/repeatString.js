e99fc1d2066f1bd9f5f96ae9bf331b9c00679b62
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2025-01-01 10:21:47 +0100

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, time) {
+    if (time < 0) {
+        return "ERROR";
+    }
+    let text = "";
+    for (i = 0; i < time; i++) {
+        text += word;
+    }
+    return text;
 };
 
 // Do not edit below this line
