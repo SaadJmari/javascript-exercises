6d354e1eb55a82a91c978784e2e494ad4ff16223
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2025-01-19 21:59:44 +0100

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((total, item) => total + item, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, item) => total * item, 1)
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let total = 1;
+  if (a == 0) {
+    return total;
+  }
+  for (let i = a; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
