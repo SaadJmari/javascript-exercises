c61a6542bf6fb2d882e29fe2bed311e45af8fcac
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2025-01-22 09:40:58 +0100

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+
+    if (num < 0) {
+        return "OOPS";
+    }
+    const actualNum = +num;
+    let fibo = new Array(actualNum);
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (let i = 2; i <= num; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+
+    return fibo[num];
 
 };
 
