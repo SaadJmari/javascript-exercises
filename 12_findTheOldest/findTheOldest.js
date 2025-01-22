f36e8c9f09a8600b1b3681bac61fc245cb1aa11d
Saad Jmari <55350721+SaadJmari@users.noreply.github.com>
2025-01-22 13:13:45 +0100

    Finish Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    const oldest = arr.reduce((a, b) => {
+        let firstPerson = a.yearOfDeath - a.yearOfBirth;
+        let secondPerson = b.yearOfDeath - b.yearOfBirth
+        if (a.yearOfDeath === undefined) {
+            firstPerson = new Date().getFullYear() - a.yearOfBirth;
+        }
+        if (b.yearOfDeath === undefined) {
+            secondPerson = new Date().getFullYear() - b.yearOfBirth;
+        }
 
+        if (firstPerson < secondPerson) {
+            return b;
+        }
+        else {
+            return a;
+        }
+    })
+    return oldest;
 };
 
 // Do not edit below this line
